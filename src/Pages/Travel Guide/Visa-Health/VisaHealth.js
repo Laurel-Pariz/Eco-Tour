309cef59c0243144234194d48e2b3a8ca088768b
Afahnyu Pila <AfahnyuPila03>
2024-06-27 19:21:02 +0200

    fix VisaHealth page

--- src/Pages/Travel Guide/Visa-Health/VisaHealth.js
+++ src/Pages/Travel Guide/Visa-Health/VisaHealth.js
@@ -6,11 +6,15 @@ export default function VisaHealth() {
     <div className="mt-10 mx-20 px-20">
       {travelInfo.map((info, index) => (
         <div key={index}>
-          <h2>{info.title}</h2>
+          <h2 className="text-gray-800 text-4xl my-4 font-medium">
+            {info.title}
+          </h2>
           {info.content.map((item, itemIndex) => (
             <div key={itemIndex}>
-              <h3>{item.subtitle}</h3>
-              <p>{item.details}</p>
+              <h3 className="my-2 text-gray-800 text-2xl font-medium">
+                - {item.subtitle}
+              </h3>
+              <p className="text-lg">{item.details}</p>
             </div>
           ))}
         </div>
