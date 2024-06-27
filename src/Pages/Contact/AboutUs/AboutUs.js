e2e38c366825f45af0c3aac28d5cc3d52f1b6cba
Afahnyu Pila <AfahnyuPila03>
2024-06-27 19:35:51 +0200

    fix About page

--- src/Pages/Contact/AboutUs/AboutUs.js
+++ src/Pages/Contact/AboutUs/AboutUs.js
@@ -4,14 +4,16 @@ import { aboutInfo } from "../../../Components/Data/data";
 export default function AboutUs() {
   return (
     <div className="mt-10 mx-20 px-20">
-      <h1>About Cameroon</h1>
+      <h1 className="text-4xl font-medium text-center my-8">
+        About <span className="text-red-500">Cameroon</span>
+      </h1>
 
       {aboutInfo.map((info, index) => (
         <div key={index}>
-          <h2>{info.title}</h2>
-          <p>{info.content}</p>
-          {index !== aboutInfo.length - 1 && <hr />}{" "}
-          {/* Add hr except for the last item */}
+          <h2 className="text-2xl text-gray-800 font-medium my-4">
+            {info.title}
+          </h2>
+          <p className="text-xl">{info.content}</p>
         </div>
       ))}
     </div>
