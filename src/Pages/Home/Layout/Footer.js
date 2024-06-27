0eb7f00d65be4cf9a6de60af4119a97656fc1004
Afahnyu Pila <AfahnyuPila03>
2024-06-27 09:14:47 +0200

    add footer section data for currYear

--- src/Pages/Home/Layout/Footer.js
+++ src/Pages/Home/Layout/Footer.js
@@ -1,3 +1,17 @@
+import React from "react";
+
 export default function Footer() {
-  return <h1>Footer Section</h1>;
+
+  const currYear = new Date().getFullYear()
+
+  return (
+    <div>
+      <h1>Footer section</h1>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div>Established since &copy;{currYear}. All copy rights reserved.</div>
+    </div>
+  );
 }
