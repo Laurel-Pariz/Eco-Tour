6b57dd25e16a0b0c7334dde57095fcd982f5725f
Afahnyu Pila <AfahnyuPila03>
2024-07-10 22:29:48 +0200

    final updates to web-app

--- src/Pages/Home/Layout/Footer.js
+++ src/Pages/Home/Layout/Footer.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { navigation } from "./Navbar";
 import { Link } from "react-router-dom";
+import { AppState } from "../../../Store/context";
 
 export default function Footer() {
   const currYear = new Date().getFullYear();
 
-  const quickLinks = navigation.filter(
+  const { user } = AppState();
+  const userRole = user?.user.user_metadata.role;
+
+  const quickLinks = navigation(userRole).filter(
     (name) =>
       name.name !== "Home" &&
       name.name !== "Auth Page" &&
@@ -15,26 +19,35 @@ export default function Footer() {
   return (
     <div className="bg-gray-800">
       <div className="mx-20 pt-8 px-20 mt-10">
-        <div>
-          <h2 className="text-2xl mb-4 text-white underline">Quick Links</h2>
-          <ul>
-            {quickLinks.map((link, index) => (
-              <li key={index} className="py-1">
-                <Link to={link.href} className="text-lg text-gray-300">
-                  {link.name}
-                </Link>
-              </li>
-            ))}
-            <li className="py-1">
-              <Link to="booked-tours" className="text-lg text-gray-300">
-                Booked Tours
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div></div>
-        <div></div>
-        <div></div>
+        {userRole !== "admin" && (
+          <>
+            {" "}
+            <div>
+              <h2 className="text-2xl mb-4 text-white underline">
+                Quick Links
+              </h2>
+              <ul>
+                {quickLinks.map((link, index) => (
+                  <li key={index} className="py-1">
+                    <Link to={link.href} className="text-lg text-gray-300">
+                      {link.name}
+                    </Link>
+                  </li>
+                ))}
+                {userRole !== "admin" && (
+                  <li className="py-1">
+                    <Link to="booked-tours" className="text-lg text-gray-300">
+                      Booked Tours
+                    </Link>
+                  </li>
+                )}
+              </ul>
+            </div>
+            <div></div>
+            <div></div>
+            <div></div>{" "}
+          </>
+        )}
         <div className="flex justify-center text-center pb-4">
           <p className="text-lg text-white">
             Established since &copy; {currYear}. All rights reserved.
